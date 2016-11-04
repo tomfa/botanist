f6bc46d8bcac8a7b8f444d6de0e6cdd9c9433f2e
Tomas Fagerbekk <tomas@webutvikling.org>
2016-11-04 10:50:32 +0100

    Fix formatted response

--- scripts/fancyrespondexample.js
+++ scripts/fancyrespondexample.js
@@ -8,22 +8,37 @@
 module.exports = function (robot) {
 	robot.respond(/fancyhello/, function(res){
 		var response = {
-			"fallback": "Required text summary of the attachment that is shown by clients that understand attachments but choose not to show them.",
-
-			"text": "Optional text that should appear within the attachment",
-			"pretext": "Optional text that should appear above the formatted data",
-
-			"color": "#36a64f", // Can either be one of 'good', 'warning', 'danger', or any hex color code
-
-			// Fields are displayed in a table on the message
-			"fields": [
-				{
-					"title": "Required Field Title", // The title may not contain markup and will be escaped for you
-					"value": "Text value of the field. May contain standard message markup and must be escaped as normal. May be multi-line.",
-					"short": false // Optional flag indicating whether the `value` is short enough to be displayed side-by-side with other values
-				}
-			]
+		    "attachments": [
+		        {
+		            "fallback": "Required plain-text summary of the attachment.",
+		            "color": "#36a64f",
+		            "pretext": "Optional text that appears above the attachment block",
+		            "author_name": "Bobby Tables",
+		            "author_link": "http://flickr.com/bobby/",
+		            "author_icon": "http://flickr.com/icons/bobby.jpg",
+		            "title": "Slack API Documentation",
+		            "title_link": "https://api.slack.com/",
+		            "text": "Optional text that appears within the attachment",
+		            "fields": [
+		                {
+		                    "title": "Priority",
+		                    "value": "High",
+		                    "short": true
+		                },
+		                {
+		                    "title": "Assignee",
+		                    "value": "Tomas",
+		                    "short": true
+		                }
+		            ],
+		            //"image_url": "http://pngimg.com/upload/fish_PNG1157.png",
+		            "thumb_url": "https://img1.etsystatic.com/017/0/6966518/il_75x75.504492969_dzp8.jpg",
+		            "footer": "Slack API",
+		            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+		            "ts": 123456789
+		        }
+		    ]
 		}
-		res.reply(response);
+		res.send(response);
 	})
 }
